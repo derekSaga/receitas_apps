b72d326ab0ff79ef00d97c511f969b67930b38ed
Derek da Silva <derek.coelho@juntossomosmais.com.br>
2022-01-03 12:41:28 -0300

    add view and solved url errors

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('receitas/', include('core.apps.site_receitas.urls')),
+    path('', include('core.apps.site_receitas.urls')),
 ]
