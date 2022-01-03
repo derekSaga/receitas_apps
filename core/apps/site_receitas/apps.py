301c86ef51d57d9e375dae382635c69240de61bf
Derek da Silva <derek.coelho@juntossomosmais.com.br>
2022-01-02 22:45:16 -0300

    app site_receitas

---
+++ core/apps/site_receitas/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SiteReceitasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_receitas'
+    verbose_name = 'Site de Receitas'
