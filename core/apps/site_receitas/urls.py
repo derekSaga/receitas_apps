201bef8e4bfdb41e5c08814694f147abe72198be
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-04 19:19:22 -0300

    add new view route to Receita

--- core/apps/site_receitas/urls.py
+++ core/apps/site_receitas/urls.py
@@ -6,5 +6,5 @@ app_name = "site_receitas"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita', views.receita, name='receita'),
+    path('<int:receita_id>', views.receita, name='receita'),
 ]
--- core/apps/site_receitas/views.py
+++ core/apps/site_receitas/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from core.apps.site_receitas.models import Receita
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
-    return render(request, "../templates/index.html")
+    receitas = Receita.objects.all()
+
+    dados = {"receitas": receitas}
+
+    return render(request, "../templates/index.html", dados)
 
 
-def receita(request):
-    return render(request, "../templates/receita.html")
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita
+    }
+    return render(
+        request, "../templates/receita.html", receita_a_exibir
+    )
