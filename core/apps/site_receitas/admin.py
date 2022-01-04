9f7241bfc74de5af22175396d96c6e295d8f9484
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-03 21:45:57 -0300

    add ReceitaAdmin

--- core/apps/site_receitas/admin.py
+++ core/apps/site_receitas/admin.py
@@ -1,3 +1,19 @@
+from core.apps.site_receitas.models import Receita
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    actions = None
+
+    list_display = ("id", "view_nome_receita", "categoria")
+
+    readonly_fields = ("date_receita",)
+
+    # save_as_continue = False
+    show_close_button = True
+
+    def view_nome_receita(self, obj):
+        return obj.nome_receita.title()
+
+    view_nome_receita.short_description = "Nome Receita"
