ba495a401d2d70ce0893c215ebd333efc55799a2
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-06 15:51:32 -0300

    new view of the Pessoa model

--- core/apps/pessoas/models.py
+++ core/apps/pessoas/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Pessoa(models.Model):
     nome = models.CharField(null=False, blank=False, max_length=200)
     email = models.EmailField(null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.nome.title()
