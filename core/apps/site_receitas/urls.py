d1f13b21b75040d242e0d8b6b9faae27cb10107b
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-06 18:40:02 -0300

    add search in all pages

--- core/apps/site_receitas/urls.py
+++ core/apps/site_receitas/urls.py
@@ -9,4 +9,5 @@ app_name = "site_receitas"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:receita_id>", views.receita, name="receita"),
+    path("buscar", views.buscar, name="buscar"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/apps/site_receitas/views.py
+++ core/apps/site_receitas/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
-    receitas = Receita.objects.order_by('-date_receita').filter(publicar=True).all()
+    receitas = Receita.objects.order_by("-date_receita").filter(publicar=True).all()
 
     dados = {"receitas": receitas}
 
@@ -13,9 +13,19 @@ def index(request):
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
 
-    receita_a_exibir = {
-        'receita': receita
-    }
-    return render(
-        request, "../templates/receita.html", receita_a_exibir
-    )
+    receita_a_exibir = {"receita": receita}
+    return render(request, "../templates/receita.html", receita_a_exibir)
+
+
+def buscar(request):
+
+    receitas = Receita.objects.order_by("-date_receita").filter(publicar=True).all()
+
+    if "search" in request.GET:
+        termo_busca = request.GET["search"]
+        if termo_busca:
+            receitas = receitas.filter(nome_receita__contains=termo_busca)
+
+    dados = {"receitas": receitas}
+
+    return render(request, "../templates/buscar.html", dados)
