40b548431771a5be479fda5c1267811c10325bb1
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-08 16:36:45 -0300

    solve route usuario cadastro

--- core/apps/usuario/urls.py
+++ core/apps/usuario/urls.py
@@ -1,11 +1,10 @@
-from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("cadastro", views.index, name="cadastro"),
+    path("cadastro", views.cadastro, name="cadastro"),
     path("login", views.login, name="login"),
     path("dashboard", views.dashboard, name="dashboard"),
     path("logout", views.logout, name="logout"),
