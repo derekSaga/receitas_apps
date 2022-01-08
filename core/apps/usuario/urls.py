8b42646149df2fec03c35d717b089fce6c64e2ab
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-08 17:01:38 -0300

    add new userspace

--- core/apps/usuario/urls.py
+++ core/apps/usuario/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "usuario"
+
 urlpatterns = [
     path("cadastro", views.cadastro, name="cadastro"),
     path("login", views.login, name="login"),
