6b1578003fbe582560755d4660c021bdd347bc6c
derekSilvaCoelho <derek.coelho@juntossomosmais.com.br>
2022-01-08 17:00:12 -0300

    fix usuario app path

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.apps.site_receitas.urls")),
-    path("usuarios/", include("core.apps.usuario.urls")),
+    path("usuario/", include("core.apps.usuario.urls")),
 ]
