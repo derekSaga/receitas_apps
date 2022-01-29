43b2daa3ec1701507b4f20d4457924da8b6ee2d9
Derek Coelho <derek.coelho@juntossosmosmais.com.br>
2022-01-28 22:05:59 -0300

    add delete recipe

--- core/apps/usuario/urls.py
+++ core/apps/usuario/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("dashboard", views.dashboard, name="dashboard"),
     path("logout", views.logout, name="logout"),
-    path("criar/receita", views.criar_receita, name="criar_receita")
+    path("criar/receita", views.criar_receita, name="criar_receita"),
+    path("deleta/<int:receita_id>", views.deleta_receita, name="deleta_receita"),
+
 ]
