f61cd4f40322381a51cb58cbdee298e75695152f
Derek Coelho <derek.coelho@juntossosmosmais.com.br>
2022-01-31 22:41:22 -0300

    add function to edit recipe

--- core/apps/usuario/urls.py
+++ core/apps/usuario/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path("logout", views.logout, name="logout"),
     path("criar/receita", views.criar_receita, name="criar_receita"),
     path("deleta/<int:receita_id>", views.deleta_receita, name="deleta_receita"),
-
+    path("edita/<int:receita_id>", views.edita_receita, name="edita_receita"),
+    path("atualiza_receita", views.atualiza_receita, name="atualiza_receita"),
 ]
