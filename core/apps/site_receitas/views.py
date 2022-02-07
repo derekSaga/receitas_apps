8483d45b6da6641e92f0d016cdba5cf53896061f
Derek Coelho <derek.coelho@juntossosmosmais.com.br>
2022-02-06 22:48:33 -0300

    add pagination

--- core/apps/site_receitas/views.py
+++ core/apps/site_receitas/views.py
@@ -1,11 +1,20 @@
 from core.apps.site_receitas.models import Receita
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db import transaction
+from django.shortcuts import get_object_or_404, redirect, render
 
 
 def index(request):
     receitas = Receita.objects.order_by("-date_receita").filter(publicar=True).all()
 
-    dados = {"receitas": receitas}
+    paginator = Paginator(receitas, 1)
+
+    page = request.GET.get("page")
+
+    receitas_por_pagina = paginator.get_page(page)
+    
+    dados = {"receitas": receitas_por_pagina}
 
     return render(request, "index.html", dados)
 
@@ -29,3 +38,69 @@ def buscar(request):
     dados = {"receitas": receitas}
 
     return render(request, "receitas/buscar.html", dados)
+
+def criar_receita(request):
+    if request.method == "POST":
+
+        user = get_object_or_404(User, pk=request.user.id)
+
+        nome_receita = request.POST.get("nome_receita")
+        ingredientes = request.POST.get("ingredientes")
+        modo_preparo = request.POST.get("modo_preparo")
+        tempo_preparo = request.POST.get("tempo_preparo")
+        rendimento = request.POST.get("rendimento")
+        categoria = request.POST.get("categoria")
+        files = request.FILES.get("foto_receita")
+
+        Receita.objects.create(
+            nome_receita=nome_receita,
+            ingredientes=ingredientes,
+            modo_preparo=modo_preparo,
+            tempo_preparo=tempo_preparo,
+            rendimento=rendimento,
+            categoria=categoria,
+            foto_receita=files,
+            pessoa=user,
+        )
+        return redirect("usuario:dashboard")
+
+    return render(request, "receitas/criar_receita.html")
+
+
+def deleta_receita(request, receita_id):
+    with transaction.atomic():
+        receita = get_object_or_404(Receita, pk=receita_id)
+
+        receita.delete()
+
+    return redirect("usuario:dashboard")
+
+
+def edita_receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_editar = {'receita': receita}
+
+    return render(request, 'receitas/edita_receita.html', receita_a_editar)
+
+
+def atualiza_receita(request):
+    if request.method == 'POST':
+
+        receita_id = request.POST.get('receita_id')
+        with transaction.atomic():
+
+            receita = get_object_or_404(Receita, pk=receita_id)
+
+            receita.nome_receita = request.POST['nome_receita']
+            receita.ingredientes = request.POST['ingredientes']
+            receita.modo_preparo = request.POST['modo_preparo']
+            receita.tempo_preparo = request.POST['tempo_preparo']
+            receita.rendimento = request.POST['rendimento']
+            receita.categoria = request.POST['categoria']
+
+            if 'foto_receita' in request.FILES:
+                receita.foto_receita = request.FILES['foto_receita']
+
+            receita.save()
+        return redirect("usuario:dashboard")
